f027fa18db8a9a78b0665168be0dc51c8fd7651b
shma2001gmailcom <shma2001@gmail.com>
2019-01-15 13:49:14 +0300

    fix left join

--- bash/deploy.sh
+++ bash/deploy.sh
@@ -9,19 +9,19 @@
 ## 5. copy new war from target dir to server webapp dir
 ## 6. start server
 ## 7. open project home page
-appname="lie-fe"
+app_name="lie-fe"
 ############### properties ####################
 ################ AT WORK ######################
 ###############################################
 #appfolder="/home/mshevelin/workspace/"${appname}"-assembla"
-#tomcatfolder="C:\Users\mikhail_shevelin\workspace\tomcat8"
-javahome="C:\jdk1.8.0_171"
+#tomcat_folder="C:\Users\mikhail_shevelin\workspace\tomcat8"
+java_home="C:\jdk1.8.0_171"
 
 ################################################
 ################ AT HOME #######################
 ################################################
-appfolder="C:/Users\mikhail_shevelin/workspace/lie-fe/"${appname}
-tomcatfolder="C:/Users/mikhail_shevelin/workspace/tomcat8"
+app_folder="C:/Users\mikhail_shevelin/workspace/lie-fe/"${app_name}
+tomcat_folder="C:/Users/mikhail_shevelin/workspace/tomcat8"
 M2_HOME='C:/maven-3.5.4'
 export M2_HOME
 M2=${M2_HOME}/bin
@@ -32,20 +32,20 @@ mvn=${M2}/mvn.cmd
 logfile='./1'
 
 ################################################
-export JAVA_HOME=${javahome}
-tomcatbin=${tomcatfolder}/bin
-tomcatwebapps=${tomcatfolder}/webapps
-if [ ! -e ${appfolder} ]; then echo 'ERROR: no appfolder' ${appfolder} 'found';exit 1; fi
-if [ ! -e ${tomcatbin} ]; then echo 'ERROR: no tomcatbin found';exit 1; fi
-cd ${appfolder}
+export JAVA_HOME=${java_home}
+tomcat_bin=${tomcat_folder}/bin
+tomcat_web_apps=${tomcat_folder}/webapps
+if [[ ! -e ${app_folder} ]]; then echo 'ERROR: no app folder' ${app_folder} 'found';exit 1; fi
+if [[ ! -e ${tomcat_bin} ]]; then echo 'ERROR: no tomcat/bin found';exit 1; fi
+cd ${app_folder}
 ${mvn} clean install $@ | tee out.txt ; test ${PIPESTATUS[0]} -eq 0
-if [ ${PIPESTATUS[0]} -ne "0" ]; then
+if [[ ${PIPESTATUS[0]} -ne "0" ]]; then
     echo ===================================================
     echo maven build failed, see output for details;exit 1;
     echo ===================================================
 fi
-cd ${tomcatbin}
-if [ "$(ps axf | grep catalina | grep -v grep)" ]; then
+cd ${tomcat_bin}
+if [[ "$(ps axf | grep catalina | grep -v grep)" ]]; then
     echo ///////////////////////////
     echo        stopping tomcat...
     bash shutdown.sh
@@ -54,25 +54,25 @@ if [ "$(ps axf | grep catalina | grep -v grep)" ]; then
     echo        tomcat has been stopped
 fi
 bash shutdown.sh
-cd ${tomcatwebapps}
-if [ -e ${tomcatwebapps}/${appname} ]; then
+cd ${tomcat_web_apps}
+if [[ -e ${tomcat_web_apps}/${app_name} ]]; then
     echo ///////////////////////////
     echo      remove old deployment...
-    rm -rf -- ${tomcatwebapps}/${appname}
+    rm -rf -- ${tomcat_web_apps}/${app_name}
 fi
-if [ -e  ${tomcatwebapps}/${appname}.war ]; then
+if [[ -e  ${tomcat_web_apps}/${app_name}.war ]]; then
     echo ///////////////////////////
     echo      remove old war...
-    rm -rf -- ${tomcatwebapps}/${appname}.war
+    rm -rf -- ${tomcat_web_apps}/${app_name}.war
 fi
 echo ///////////////////////////
 echo      deploying new version...
-cp  ${appfolder}/target/${appname}.war ${tomcatwebapps}/${appname}.war
-cd ${tomcatbin}
+cp  ${app_folder}/target/${app_name}.war ${tomcat_web_apps}/${app_name}.war
+cd ${tomcat_bin}
 echo ///////////////////////////
 echo         starting tomcat...
 echo ///////////////////////////
 bash startup.sh
 sleep 5
 # firefox "http://localhost:8080/"${appname}
-"C:/Program Files (x86)/Google/Chrome/Application/chrome.exe" "http://localhost:8080/"${appname}
+"C:/Program Files (x86)/Google/Chrome/Application/chrome.exe" "http://localhost:8080/"${app_name}
